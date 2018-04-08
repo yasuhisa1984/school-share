df2a3aa697b50c95638041afb45e7ad226a32b1b
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-08 16:18:53 +0900

    修正 タイムゾーン及びデフォルトのコメント

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,15 @@
 require_relative 'boot'
 require 'rails/all'
 Bundler.require(*Rails.groups)
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+
 
 module SchoolShare
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
+
     config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
     config.generators do |g|
@@ -16,5 +19,8 @@ module SchoolShare
       g.controller_specs false
       g.view_specs false
     end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
