43a9350d70ef084d5f16f7a950ea83a6f06a999d
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-10 14:08:03 +0900

    追加 学校のSeedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+Faker::Config.locale = :ja
+
+#スクール
+5.times {
+  name = ['プログラミングスクールA','プログラミングスクールB','ITスクールC','ITスクールD','パソコンスクールE'].sample
+  url = Faker::Internet.url
+  description = Faker::Hacker.say_something_smart
+  school_image_url = 'gakkou.png'
+  remote = [true,false].sample
+
+  School.create!(
+    name: "#{name}",
+    url: "#{url}",
+    description: "#{description}",
+    school_image_url: "#{school_image_url}",
+    remote: "#{remote}"
+  )
+}
