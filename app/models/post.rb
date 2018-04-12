d1e8e1398bc2119ca95968c717349355fe31d9a8
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-12 16:49:09 +0900

    追加 Postsテーブルに外部キー

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :users, optional: true
   has_many :post_schools, dependent: :destroy
   has_many :schools, through: :post_schools
   validates :story, presence: true, length: { maximum: 8000 }
