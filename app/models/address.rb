42c339dd6f84970b310757252b4bb76e7f30273b
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-13 18:41:56 +0900

    修正 リレーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,3 @@
 class Address < ApplicationRecord
-  belongs_to :schools, optional: true
+  belongs_to :school
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,3 @@
 class Course < ApplicationRecord
-  belongs_to :schools, optional: true
+  belongs_to :school
 end
