fd265e644b2a81002eaa10414c29d99dfd18afc3
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-13 11:52:19 +0900

    追加 coursesへのリレーション関係の設定

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,5 +2,6 @@ class School < ApplicationRecord
   has_many :post_schools, dependent: :destroy
   has_many :posts, through: :post_schools
   has_many :addresses, dependent: :destroy
+  has_many :courses, dependent: :destroy
   validates :name, presence: true, length: { maximum: 30 }
 end
