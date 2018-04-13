f2dfad7cbc11edf79aa16271d79e43d2099db23b
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-13 10:56:52 +0900

    追加 addressesへのリレーション関係の設定

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,6 @@
 class School < ApplicationRecord
   has_many :post_schools, dependent: :destroy
   has_many :posts, through: :post_schools
+  has_many :addresses, dependent: :destroy
   validates :name, presence: true, length: { maximum: 30 }
 end
