a208d5ca123e966be107fc80ba085aceb0418cab
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-13 11:03:29 +0900

    追加 学校の詳細ページ

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,4 +2,9 @@ class SchoolsController < ApplicationController
   def index
     @schools = School.all
   end
+
+  def show
+    @school = School.find(params[:id])
+    @addresses = @school.addresses
+  end
 end
