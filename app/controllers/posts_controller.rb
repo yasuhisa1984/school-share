53c197aae092cecf33ed8ef0986adc337bf0bf52
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-16 11:50:56 +0900

    修正 投稿の詳細画面の設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+    @skills = @post.skills
+    @purposes = @post.purposes
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
   has_many :post_schools, dependent: :destroy
   has_many :schools, through: :post_schools
+
+  has_many :post_skills, dependent: :destroy
+  has_many :skills, through: :post_skills
+
+  has_many :post_purposes, dependent: :destroy
+  has_many :purposes, through: :post_purposes
+
+  has_many :post_schools, dependent: :destroy
+  has_many :schools, through: :post_schools
   validates :story, presence: true, length: { maximum: 8000 }
 end
