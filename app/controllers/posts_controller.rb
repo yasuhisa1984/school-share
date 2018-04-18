24857a010061e75972de2d1b856e8624221f5727
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-04-18 12:21:37 +0900

    追加 学校をプルダウンで選択

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show]
+  before_action :set_schools, only: [:new, :create]
 
   def index
     @posts = Post.all
@@ -8,7 +9,6 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    @schools = School.all
   end
 
   def show
@@ -21,6 +21,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
     @post.skills.build(name: post_params[:skills])
     @post.purposes.build(name: post_params[:purposes])
+    @post.post_schools.build(school_id: post_params[:schools])
     if @post.save
       redirect_to root_path, notice: "投稿しました"
     else
@@ -31,6 +32,10 @@ class PostsController < ApplicationController
   private
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def set_schools
+    @schools = School.all
   end
 
   def post_params
