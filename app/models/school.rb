5a1a7d6df1b4c830cfe1179624abfad779ea7ccb
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-05-17 11:22:23 +0900

    修正 README表記

--- app/models/school.rb
+++ app/models/school.rb
@@ -7,11 +7,4 @@ class School < ApplicationRecord
   has_many :purposes, through: :school_purposes
 
   validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
-
-  private
-  def normalize_values
-    self.family_name_kana = normalize_as_furigana(family_name_kana)
-    self.given_name_kana = normalize_as_furigana(given_name_kana)
-    self.postal_code = normalize_as_postal_code(postal_code)
-  end
 end
