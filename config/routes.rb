735b6059963fc5957f7d546468df9f8ab0cca249
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-06-13 18:41:59 +0900

    追加 お問い合わせフォーム

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'tops#index'
 
   devise_for :users, controllers: {
+    registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
@@ -10,16 +11,23 @@ Rails.application.routes.draw do
   resources :users, only: %i[show]
 
   resources :schools, only: %i[index show] do
-    resources :posts, only: %i[index new show create destroy], module: 'schools'
+    resources :posts, only: %i[index new show create destroy edit update], module: 'schools'
     collection do
       get :search
     end
   end
 
   resources :posts, only: %i[index new show create destroy] do
+    resources :scores, only: %i[new create destroy edit]
     collection do
       get :search
       get :search_post
+    end
+  end
+
+  resources :contacts, only: [:new, :create] do
+    collection do
+      post :confirm
     end
   end
 
