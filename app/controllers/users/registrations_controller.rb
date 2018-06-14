c615a906ef985f3eae6e09cab2714b3de9e87725
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-06-14 20:33:34 +0900

    修正 SignUpページエラー

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def build_resource(hash=nil)
+  def build_resource(hash={})
     hash[:uid] = User.create_unique_string
     super
   end
