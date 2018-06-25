a894976de22e927a4fdc5b07467268435b2ff5cb
小鍛冶泰久 <penmouse275@shoutanyayasuhisa-no-MacBook-Pro.local>
2018-06-25 11:33:14 +0900

    修正 リレーション表記ミス

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -1,6 +1,6 @@
 class Purpose < ApplicationRecord
   has_many :post_purposes, dependent: :destroy
-  has_many :posts, through: :post_purpose
+  has_many :posts, through: :post_purposes
 
   has_many :school_purposes, dependent: :destroy
   has_many :schools, through: :school_purposes
